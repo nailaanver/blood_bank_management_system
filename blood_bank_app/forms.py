64736166b64b05bd748e2542db2e824427871f4e
Fathima Naila <fathimanaila71@gmail.com>
2025-10-13 12:05:46 +0530

    initial commit

---
+++ blood_bank_app/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    role = forms.ChoiceField(choices=Profile.ROLE_CHOICES)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        help_texts = {
+            'username': None,   # removes "Required. 150 characters..." text
+        }
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+        return cleaned_data
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
---
+++ blood_bank_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('donor', 'Donor'),
+        ('patient', 'Patient'),
+        ('hospital','Hospital'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES,default='--Choose Role--')
+    def __str__(self):
+        return f"{self.user} ({self.role})"
